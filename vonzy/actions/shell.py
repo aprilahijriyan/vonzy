e314e868c566a80db0b6758047566122fd3892b9
aprilahijriyan <hijriyan23@gmail.com>
2023-08-28 03:27:47 +0700

    🎈 perf(action): improved reading results from shell commands

--- vonzy/actions/shell.py
+++ vonzy/actions/shell.py
@@ -1,4 +1,5 @@
 import ast
+import contextlib
 import functools
 import os
 import re
@@ -6,7 +7,6 @@ import shutil
 import typing
 
 import pexpect
-import pexpect.popen_spawn
 from pydantic import PrivateAttr
 
 from ..logger import log
@@ -28,7 +28,7 @@ def clean(s):
     """
     strip = re.compile(r"(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]")
     txt = strip.sub("", s)
-    return txt.replace("\r", "").replace("\n", "")
+    return txt  # .replace("\r", "").replace("\n", "")
 
 
 def _validate_return_code(fn):
@@ -36,12 +36,9 @@ def _validate_return_code(fn):
     def wrapper(*args, **kwargs):
         instance: Action = args[0]
         rv = fn(*args, **kwargs)
-        if not kwargs.get("expect"):
+        if not instance.is_controlled:
             log.debug(f"Getting return code for cmd={args[1]}")
-            results = fn(
-                instance,
-                cmd="echo $?",
-            )
+            results = fn(instance, cmd="echo $?", print_fn=None)
             log.debug(f"cmd={args[1]} results={results!r}")
             results = clean("".join(results))
             returncode = None
@@ -60,9 +57,11 @@ def _validate_return_code(fn):
 class Action(BaseAction):
     cwd: typing.Optional[str] = None
     debug: typing.Optional[bool] = False
-    timeout: typing.Optional[float] = 2
+    timeout: typing.Optional[float] = 5
     _command: str = PrivateAttr(DEFAULT_SHELL)
-    _process: typing.Optional[pexpect.spawn] = PrivateAttr(None)
+    _process: typing.Optional[pexpect.pty_spawn.spawn] = PrivateAttr(None)
+    _control_output: bool = PrivateAttr(False)
+    _should_print: bool = PrivateAttr(False)
 
     def initialize(self) -> None:
         log.debug(f"Launches the command {self._command!r} into a background process.")
@@ -75,6 +74,7 @@ class Action(BaseAction):
                 env=os.environ,
             )
             self._process.delaybeforesend = 0.1
+            self._process.delayafterread = 0.1
             # self._process.logfile = sys.stdout
 
     def cleanup(self) -> typing.Optional[int]:
@@ -85,6 +85,19 @@ class Action(BaseAction):
             exit_code = self._process.exitstatus
             self._process = None
         log.debug(f"Process {self._command!r} exited with code {exit_code!r}")
+        return exit_code
+
+    @contextlib.contextmanager
+    def control_output(self):
+        self._control_output = True
+        try:
+            yield self
+        finally:
+            self._control_output = False
+
+    @property
+    def is_controlled(self) -> bool:
+        return self._control_output
 
     @_validate_return_code
     def execute(
@@ -94,42 +107,47 @@ class Action(BaseAction):
         expect: typing.Optional[str] = None,
         context: typing.Optional["StepContext"] = None,
         line_callback: typing.Optional[typing.Callable[[str], None]] = None,
+        print_fn: typing.Optional[typing.Callable] = print,
     ):
         if not isinstance(cmd, str):
             raise RuntimeError(f"{__name__}: Command {cmd!r} is not a string.")
 
         if context is not None:
             cmd = render_step_context(cmd.strip(), context=context)
+
+        default_expect = ["\r\n", "\n"]
         if expect:
             log.debug(f"Expects {expect!r} on stdin")
             self._process.expect(expect)
-
-        if not expect:
+        else:
             # Hide the log if it has the `expect` param. To prevent displaying unwanted text/data on the console.
             log.debug(f"Executing command {cmd!r}")
-            expect = ["\r", "\n"]
 
         self._process.sendline(cmd)
-        should_print = False
         lines = []
+        _should_print = self._should_print if self.is_controlled else False
         while True:
             try:
-                self._process.expect(expect)
-                if should_print:
-                    line = self._process.before + self._process.after
-                    if self.debug:
-                        print(line, end="")
+                self._process.expect(default_expect)
+                line = self._process.before + self._process.after
+                # print(f"should_print={_should_print} match_idx={match_idx!r} line={line!r}")
+                if _should_print:
+                    if isinstance(line, str):
+                        line = clean(line)
+                    if self.debug and callable(print_fn):
+                        print_fn(line, end="")
                     if callable(line_callback):
                         line_callback(line)
                     lines.append(line)
 
-                if self._process.before == "\x1b[?2004l":
-                    should_print = True
+                if self._process.before and self._process.before.startswith(
+                    "\x1b[?2004h"
+                ):
+                    _should_print = True
+                    if self.is_controlled:
+                        self._should_print = _should_print
             except (pexpect.TIMEOUT, pexpect.EOF) as e:
                 # log.error(f"Command {cmd!r} timed out/eof. {e}")
-                break
-            except Exception as e:
-                log.error(f"Error executing command {cmd!r}: {e}")
                 break
 
         return lines
