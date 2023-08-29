92c8a1c40f6189408933a15b0fadbe8fda457994
aprilahijriyan <hijriyan23@gmail.com>
2023-08-29 15:32:25 +0700

    ✨ feat(cli): added -e option to override environment variables

--- vonzy/__main__.py
+++ vonzy/__main__.py
@@ -1,10 +1,16 @@
+import functools
 import typing
-from pathlib import Path
 
+from click import Context as ClickContext
 from rich import print, tree
 from typer import Context, FileText, Option, Typer
 
 from .schema import Step, Workflow
+
+try:
+    from dotenv import load_dotenv
+except ImportError:
+    load_dotenv = None
 
 app = Typer(
     name="vonzy",
@@ -13,16 +19,33 @@ app = Typer(
 )
 
 
+def required_workflow(fn):
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        ctx: ClickContext = None
+        for value in kwargs.values():
+            if isinstance(value, ClickContext):
+                ctx = value
+                break
+        if isinstance(ctx, ClickContext):
+            workflow = ctx.obj
+            if not isinstance(workflow, Workflow):
+                print(
+                    f"Error: no workflow found. use 'vonzy -c ...' option to load workflow file."
+                )
+                ctx.abort()
+        return fn(*args, **kwargs)
+
+    return wrapper
+
+
 @app.callback(invoke_without_command=True)
 def main(
     ctx: Context,
-    config: FileText = Option(
-        Path.cwd() / "vonzy.yml", "-c", "--config", help="Configuration file"
-    ),
+    config: FileText = Option(None, "-c", "--config", help="Configuration file"),
 ):
     if not config:
-        print("No config file found")
-        ctx.abort()
+        return
 
     try:
         workflow = Workflow.parse_config(config)
@@ -33,6 +56,7 @@ def main(
 
 
 @app.command()
+@required_workflow
 def run(
     ctx: Context,
     step_ids: typing.Optional[list[str]] = Option(
@@ -41,16 +65,32 @@ def run(
         "--step",
         help="Step IDs to run",
     ),
+    env_file: typing.Optional[list[str]] = Option(
+        None,
+        "-e",
+        "--env",
+        help="Environment variables file (dotenv format)",
+    ),
 ):
     """
     Run workflow
     """
+
+    if load_dotenv is None and env_file:
+        print(
+            f"Error: you used the '-e' option but you haven't installed the python-dotenv module."
+        )
+        ctx.abort()
+
+    for f in env_file:
+        load_dotenv(f, override=True)
 
     workflow: Workflow = ctx.obj
     list(workflow.run(step_ids=step_ids))
 
 
 @app.command()
+@required_workflow
 def steps(
     ctx: Context,
 ):
